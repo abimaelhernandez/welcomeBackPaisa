f6b5b8091009e42b8b9f121aa1726fc68c9b4992
Eduardo091 <urrutia.e14@gmail.com>
2018-03-03 02:41:02 -0600

    for the revase

---
+++ client/src/components/Thread.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Thread extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (<div>
+           {this.props.thread.comment}
+         <button >Reply</button>
+
+      </div>
+    )
+  }
+}
+
+export default Thread;
